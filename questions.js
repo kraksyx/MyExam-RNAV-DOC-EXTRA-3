01394372ae98693bd8835d06e0176b4002b7092d
kraksyx <blahblahusdme@gmail.com>
2025-08-05 21:42:18 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,73 @@
+const questions = [
+  {
+    "question": "Which of the following statements concerning DGPS is true?",
+    "options": [
+      "Wide area DGPS (WADGPS) accuracy improves the closer it is to a ground station",
+      "The local area DGPS gives the same improvement in accuracy regardless of the distance to the station",
+      "DGPS removes SV and clock errors and propagation errors",
+      "DGPS can improve the accuracy of the position information"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "The CS25 regulation color code rules for EFIS display recommend color for selected headings to be:",
+    "options": [
+      "Cyan",
+      "White",
+      "Green",
+      "Magenta"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which of the following statements about the accuracy that can be obtained with DGPS of satellite navigation systems is correct?",
+    "options": [
+      "A DGPS receiver can detect and correct for multi-path error, providing a more accurate position.",
+      "Only DGPS allows position fixes accurate enough for non-precision approaches",
+      "The increase in accuracy of position fixes is independent of the receiver position in relation to the ground station.",
+      "The nearer a receiver is situated to the DGPS ground station, the more accurate the position"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "What most affects the range available for secondary radar?",
+    "options": [
+      "The transmission power of the ground transponder",
+      "The transmission power of the interrogator",
+      "PRB",
+      "The height of the aircraft and the height of the ground interrogator"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which satellite NAVSTAR GPS navigation errors are corrected for by the DGPS technique?",
+    "options": [
+      "Ephemeris and receiver clock",
+      "Receiver noise",
+      "Clock error and Selective Availability",
+      "Clock and receiver noise"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Which statement about the interrogation by the DME interrogator is correct?",
+    "options": [
+      "The interrogation can only take place if the echo protection circuit has been locked",
+      "The interrogation does not start before the pulse pairs of the tuned DME are received",
+      "The interrogation starts when the interrogator has been warmed up sufficiently whether the pulse pairs are received or not",
+      "The interrogation starts directly after the correct DME frequency has been selected on the frequency selector panel by the pilot"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "When using a two dot HSI, a deviation of one dot from the computed track",
+    "options": [
+      "2 NM",
+      "5 degrees",
+      "2 degrees",
+      "5 NM"
+    ],
+    "correct": 0
+  }
+];
+export default questions;
